4309aed01e24a9b03a5bf51515602b44a196eb06
Harikrishnan R T <104484586+Harikrishnanrt@users.noreply.github.com>
2022-11-09 12:11:10 +0530

    lab2.sql

--- lab2.sql
+++ lab2.sql
@@ -1,47 +1,58 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Nov 09, 2022 at 07:18 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 7.4.29
+CREATE TABLE IF NOT EXISTS users( 
+	id int NOT NULL AUTO_INCREMENT, 
+	name VARCHAR(100) NOT NULL, 
+	email VARCHAR(100) NOT NULL, 
+  	password varchar(50) NOT NULL,
+	PRIMARY KEY(id)
+);
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+CREATE TABLE IF NOT EXISTS agents( 
+	id int NOT NULL AUTO_INCREMENT, 
+	name VARCHAR(100) NOT NULL, 
+	email VARCHAR(100) NOT NULL, 
+  	password varchar(50) NOT NULL,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS employees( 
+	id int NOT NULL AUTO_INCREMENT, 
+	name VARCHAR(100) NOT NULL, 
+	email VARCHAR(100) NOT NULL, 
+  	password varchar(50) NOT NULL,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS complaints( 
+	id INT NOT NULL AUTO_INCREMENT, 
+	user_id INT, 
+	agent_id INT, 
+	employee_id INT, 
+  	complaint text NOT NULL,
+  	status VARCHAR(20) NOT NULL,
+  	`date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY(id), 
+	FOREIGN KEY(user_id) REFERENCES users(id),
+	FOREIGN KEY(agent_id) REFERENCES agents(id),
+	FOREIGN KEY(employee_id) REFERENCES employees(id)
+);
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `lab2`
---
 
--- --------------------------------------------------------
 
---
--- Table structure for table `register`
---
 
-CREATE TABLE `register` (
-  `uid` int(10) NOT NULL,
-  `firstname` varchar(255) DEFAULT NULL,
-  `lastname` varchar(255) DEFAULT NULL,
-  `username` varchar(255) DEFAULT NULL,
-  `dob` date DEFAULT NULL,
-  `gender` enum('male','female') DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `mobileno` varchar(255) DEFAULT NULL,
-  `password` varchar(50) DEFAULT NULL,
-  `repeatpassword` varchar(50) DEFAULT NULL,
-  `complaints` varchar(250) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
